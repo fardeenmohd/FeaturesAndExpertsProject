a809ef72bddef4559689472df249c64a5c1daa64
matrackif <filip.matracki@gmail.com>
2017-10-21 22:53:24 +0200

    Added first version of brute force solution

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
 import argparse
 import csv
+import itertools
+from typing import List
+
+
+class Solution:
+    def __init__(self, experts_used: List[int], projects_solved: List[int]):
+        self.experts_used = experts_used
+        self.num_of_experts_used = len(experts_used)
+        self.projects_solved = projects_solved
+        '''
+        for i in range(len(projects)):
+            if all([val == 0 for val in projects[i]]):
+                self.projects_solved.append(projects[i])
+        '''
+
+        self.num_of_projects_solved = len(self.projects_solved)
+
+    def print_result(self):
+        print("--SOLUTION RESULT--")
+        print("Number of projects solved: " + str(self.num_of_projects_solved))
+        print("Number of experts used: " + str(self.num_of_experts_used))
+        print("Experts used: " + str(self.experts_used))
+        print("Projects solved: " + str(self.projects_solved))
 
 
 class Main:
@@ -11,6 +34,9 @@ class Main:
         self.number_of_features = 0
         self.experts = []
         self.projects = []
+        self.expert_combinations = []
+        self.project_combinations = []
+        self.solutions = []
 
     def print_everything(self):
         print("Input data parsed:")
@@ -46,6 +72,47 @@ class Main:
         for x in range(self.number_of_projects + 1, self.number_of_projects + self.number_of_experts + 1):
             self.experts.append(list(map(int, self.data[x])))
 
+    def solve_brute_force(self):
+        expert_indices = [i for i in range(len(self.experts))]  # Get list of indices of experts
+        project_indices = [i for i in range(len(self.projects))]
+
+        # Find every combination of expert indices (of length len(self.experts))
+        self.expert_combinations = list(itertools.permutations(expert_indices, len(self.experts)))
+
+        # Find every combination of project indices
+        for length in range(1, len(self.projects) + 1):
+            for subset in itertools.permutations(project_indices, length):
+                self.project_combinations.append(list(subset))
+
+        # Loop through every combination of experts and projects and find a solution
+        for expert_combination in self.expert_combinations:
+            for project_combination in self.project_combinations:
+                # Copy list of experts and projects to a temp variable so we can distinguish between solutions
+                current_experts = list([list(self.experts[i]) for i in expert_combination])
+                current_projects = list([list(self.projects[i]) for i in project_combination])
+                print("Current Projects: " + str(current_projects))
+                print("Current experts: " + str(current_experts))
+                expert_idx = 0
+                experts_used = []
+                projects_solved = []
+                for expert in current_experts:
+                    expert_was_used = False
+                    project_idx = 0
+                    # Try to apply expert to all projects
+                    for project in current_projects:
+                        if not expert_was_used:
+                            for j in range(self.number_of_features):
+                                if project[j] > 0 and expert[j] > 0:
+                                    project[j] -= 1
+                                    # expert[j] = 0  # So this expert does not get used again
+                                    expert_was_used = True
+                                    experts_used.append(expert_combination[expert_idx])
+                                    if all([val == 0 for val in project]):
+                                        projects_solved.append(project_combination[project_idx])
+                                    break
+                        project_idx += 1
+                    expert_idx += 1
+                self.solutions.append(Solution(experts_used, projects_solved))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -57,3 +124,6 @@ if __name__ == '__main__':
     program_args = vars(parser.parse_args())
     main = Main(program_args['file'])
     main.read_csv()
+    main.solve_brute_force()
+    for solution in main.solutions:
+        solution.print_result()
